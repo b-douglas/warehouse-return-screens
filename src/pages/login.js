3f58d5cb30d63c830bd648f7a267c8cf4c197609
Doug Robinson <33792770+b-douglas@users.noreply.github.com>
2020-03-28 19:05:51 -0400

    Sat-Mar28-1905 dougrob updates to repository

--- src/pages/login.js
+++ src/pages/login.js
@@ -3,81 +3,108 @@ import { Link, navigate } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import styles from './login.module.scss';
-
+import styles from "./login.module.scss"
 
 export default class LoginPage extends React.Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
       userid: "enter your user id",
       password: "place",
-    };
+    }
 
-    this.handleInputChange = this.handleInputChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
   }
 
   handleInputChange(event) {
-    const target = event.target;
-    const value = target.value;
-    const name = target.name;
+    const target = event.target
+    const value = target.value
+    const name = target.name
 
     this.setState({
-      [name]: value
-    });
+      [name]: value,
+    })
   }
 
-  handleSubmit = event => {
-    event.preventDefault();
-    alert(`Call API with - ${this.state.userid} ${this.state.password}!`);
-    navigate("/returninput");
+  handleSubmit(event) {
+    event.preventDefault()
+    alert(`Call API with - ${this.state.userid} ${this.state.password}!`)
+    navigate("/returninput")
   }
 
   render() {
     return (
       <Layout>
-      <SEO title="Login" />
+        <SEO title="Login" />
 
-
-      {/*
+        {/*
       Begin of Custom HTML
       */}
 
-      <h2>Sign in <br /> to continue</h2>
+        <h2>
+          Sign in <br /> to continue
+        </h2>
 
-      <div className={styles.login}>
-      <form onSubmit={this.handleSubmit} method="POST">
-      <div class="formField formFieldInputFocus">
-        <b><label for="userid">Username </label></b>
-       <input  type="text" id="userid" name="userid" value={this.state.userid} onChange={this.handleInputChange} /><br />
-      </div>
-      <div class="formField">
-        <b><label for="password">Password </label></b>
-        <input type="password" id="password" name="password" value={this.state.password} onChange={this.handleInputChange} />
-      </div>
-      <div class="formField">
-            <label></label>
-            <input className={styles.submit}  id="SubmitLogin" type="submit" value="Log In" />
-        </div>
-      <div id="LogInLinks">
-            <a href="https://staging-admin-804279-newell-app.deckcommerce.net/Account/ForgotUsername">Forgot Username?</a> &#124;
-            <a href="https://staging-admin-804279-newell-app.deckcommerce.net/Account/ForgotPassword">Forgot Password?</a>
-        </div>
-      </form>
+        <div className={styles.login}>
+          <form onSubmit={this.handleSubmit} method="POST">
+            <div class="formField formFieldInputFocus">
+              <b>
+                <label htmlFor="userid">Username </label>
+              </b>
+              <input
+                type="text"
+                id="userid"
+                name="userid"
+                value={this.state.userid}
+                onChange={this.handleInputChange}
+              />
+              <br />
+            </div>
+            <div class="formField">
+              <b>
+                <label htmlFor="password">Password </label>
+              </b>
+              <input
+                type="password"
+                id="password"
+                name="password"
+                value={this.state.password}
+                onChange={this.handleInputChange}
+              />
+            </div>
+            <div class="formField">
+              <label>
+              <input
+                className={styles.submit}
+                id="SubmitLogin"
+                type="submit"
+                value="Log In"
+              /></label>
+            </div>
+            <div id="LogInLinks">
+              <a href="https://staging-admin-804279-newell-app.deckcommerce.net/Account/ForgotUsername">
+                Forgot Username?
+              </a>{" "}
+              &#124;
+              <a href="https://staging-admin-804279-newell-app.deckcommerce.net/Account/ForgotPassword">
+                Forgot Password?
+              </a>
+            </div>
+          </form>
 
-      <p>Note: This page will login into OMS via OAUTH.  Each warehouse returns specialist will have an account that has access to orders and make changes to returns.</p>
-      </div>
-      {/*
+          <p>
+            Note: This page will login into OMS via OAUTH. Each warehouse
+            returns specialist will have an account that has access to orders
+            and make changes to returns.
+          </p>
+        </div>
+        {/*
       End of Custom HTML
       */}
 
-
-      <Link to="">Go back to the homepage</Link>
+        <Link to="/">Go back to the homepage</Link>
       </Layout>
-    );
+    )
   }
 }
-
-
-
